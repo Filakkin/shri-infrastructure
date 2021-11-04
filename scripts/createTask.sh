2146f135641470598a025c372cd2924a88a3d3b0
filakkin <filakkin@gmail.com>
2021-11-04 20:03:52 +0700

    Добавит вывод сгенерированого jq json в скипте создания задачи

--- scripts/createTask.sh
+++ scripts/createTask.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo $(jq --arg key0 'queue' --arg value0 'TMP' --arg key1 'summary' --arg value1 '$1' '. | .[$key0]=$value0 | .[$key1]=$value1' <<< '{}')
+
 curl \
 -H "Authorization: OAuth $TOKEN" \
 -H "X-Org-ID:$ORG_ID" \
