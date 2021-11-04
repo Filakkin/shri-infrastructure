061f5358818d5199e7e0135dfec7ad8036928202
filakkin <filakkin@gmail.com>
2021-11-04 21:39:23 +0700

    Доработает отправку параметров запроса при помощи json-файла

--- scripts/createTask.sh
+++ scripts/createTask.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-JSON_PAYLOAD=$(jq -c --arg key0 'queue' --arg value0 'TMP' --arg key1 'summary' --arg value1 "$1" '. | .[$key0]=$value0 | .[$key1]=$value1' <<< '{}')
-
-JSON_PAYLOAD > 
-
 FALLBACK="{\"queue\":\"TMP\",\"summary\":\"$1\",\"assignee\":[\"259985626\"]}"
 
-echo "jq    ='$JSON_PAYLOAD'"
+echo "jq    ='$(jq . -c $2)'"
 echo "manuel='$FALLBACK'"
 
 curl \
+-X POST \
 -H "Authorization: OAuth $TOKEN" \
 -H "X-Org-ID:$ORG_ID" \
 -H "Content-Type: application/json" \
--d "@$2" \
--X POST \
+-d @$2 \
 https://api.tracker.yandex.net/v2/issues/
