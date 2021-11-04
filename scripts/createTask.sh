c5ad80324a8a943e84ded6f8b21ccceb533ddd5b
filakkin <filakkin@gmail.com>
2021-11-04 20:50:56 +0700

    Исправит скрипт поиска версии релиза
    Раньше скрипт искал последний тэг по всему репозиторию, а не по собираемой ветке

--- scripts/createTask.sh
+++ scripts/createTask.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
- JSON_PAYLOAD=$(jq -c --arg key0 'queue' --arg value0 'TMP' --arg key1 'summary' --arg value1 "$1" '. | .[$key0]=$value0 | .[$key1]=$value1' <<< '{}')
+JSON_PAYLOAD=$(jq -c --arg key0 'queue' --arg value0 'TMP' --arg key1 'summary' --arg value1 "$1" '. | .[$key0]=$value0 | .[$key1]=$value1' <<< '{}')
+
+FALLBACK="{\"queue\":\"TMP\",\"summary\":\"$1\"}"
 
 curl \
 -H "Authorization: OAuth $TOKEN" \
 -H "X-Org-ID:$ORG_ID" \
 -H "Content-Type: application/json" \
--d "{\"queue\":\"TMP\",\"summary\":\"$1\"}" \
+-d "'$JSON_PAYLOAD'" \
 -X POST \
 https://api.tracker.yandex.net/v2/issues/
--- scripts/releaseTag.sh
+++ scripts/releaseTag.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo $(git tag --sort=-taggerdate | grep -E 'v[0-9]+\.[0-9]+(\.[0-9]+)?' | sed -n 1p)
+echo $(git tag --sort=-taggerdate --merged | grep -E 'v[0-9]+\.[0-9]+(\.[0-9]+)?' | sed -n 1p)
