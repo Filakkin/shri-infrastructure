6700a90c244f361972d94a8d77190b10a1b70c53
filakkin <filakkin@gmail.com>
2021-11-04 20:00:57 +0700

    Обновит скрипт создания задачи в Я.Трекере

--- scripts/createTask.sh
+++ scripts/createTask.sh
@@ -3,6 +3,6 @@
 curl \
 -H "Authorization: OAuth $TOKEN" \
 -H "X-Org-ID:$ORG_ID" \
--d "{\"queue\":\"TMP\",\"summary\":\"$1\"}" \
+-d "$(jq --arg key0 'queue' --arg value0 'TMP' --arg key1 'summary' --arg value1 '$1' '. | .[$key0]=$value0 | .[$key1]=$value1' <<< '{}')" \
 -X POST \
 https://api.tracker.yandex.net/v2/issues/
