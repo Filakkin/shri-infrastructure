89509df91607bfd0145811745c68601c1af74b40
filakkin <filakkin@gmail.com>
2021-11-04 23:29:22 +0700

    Добавит сodename релиза в название задачи Я.Трекера

--- scripts/genCreateTaskJson.sh
+++ scripts/genCreateTaskJson.sh
@@ -3,7 +3,8 @@
 APP_NAME=$(jq -r .id ../config/main.json)
 RELEASE_TAG=$(./releaseTag.sh)
 PREV_RELEASE=$(./prevRelease.sh $RELEASE_TAG)
-RELEASE_NAME=$(./genReleaseName.sh $RELEASE_TAG)
+RELEASE_CODENAME=$(git for-each-ref --format="%(body)" "refs/tags/$RELEASE_TAG")
+RELEASE_NAME="$(./genReleaseName.sh $RELEASE_TAG) - \"$RELEASE_CODENAME\""
 RELEASE_DATE=$(git for-each-ref --format="%(taggerdate:format:%d-%m-%Y %H:%M)" "refs/tags/$RELEASE_TAG")
 CHANGELOG=$(./getChanges.sh $RELEASE_TAG $PREV_RELEASE)
 DESCRIPTION="
