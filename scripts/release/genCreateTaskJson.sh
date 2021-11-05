959bbde21c29009df629555fa8a33505ccb2430a
filakkin <filakkin@gmail.com>
2021-11-05 23:12:01 +0700

    Добавит в описание задачи кодовое имя релиза

--- scripts/release/genCreateTaskJson.sh
+++ scripts/release/genCreateTaskJson.sh
@@ -5,15 +5,17 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 APP_NAME=$(jq -r .id ../../config/main.json)
+#TODO вынести генерацию описания задачи в отдельный скрипт, так как эта часть дублирована в genUpdateTaskJson.sh
 RELEASE_TAG=$RELEASE_VERSION
 RELEASE_CODENAME=$(git for-each-ref --format="%(subject)" "refs/tags/$RELEASE_TAG")
 echo "git for-each-ref --format=\"%(taggerdate:format:%d-%m-%Y %H:%M)\" \"refs/tags/$RELEASE_TAG\""
+echo $(git for-each-ref "refs/tags/$RELEASE_TAG")
 RELEASE_DATE=$(git for-each-ref --format="%(taggerdate:format:%d-%m-%Y %H:%M)" "refs/tags/$RELEASE_TAG")
 CHANGELOG=$(./getChanges.sh $RELEASE_TAG $PREV_RELEASE)
 DESCRIPTION="
 **Версия**: $RELEASE_TAG
 **Создан**: $RELEASE_DATE
-**Codename**:
+**Codename**: $RELEASE_CODENAME
 **Список изменений**:
 #|
 || **Изменение** | **Коммит** | **Автор** ||
--- scripts/release/genUpdateTaskJson.sh
+++ scripts/release/genUpdateTaskJson.sh
@@ -4,6 +4,7 @@ current_path=$(pwd)
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+#TODO вынести генерацию описания задачи в отдельный скрипт, так как эта часть дублирована в genCreateTaskJson.sh
 RELEASE_TAG=$RELEASE_VERSION
 RELEASE_DATE=$(git for-each-ref --format="%(taggerdate:format:%d-%m-%Y %H:%M)" "refs/tags/$RELEASE_TAG")
 RELEASE_CODENAME=$(git for-each-ref --format="%(subject)" "refs/tags/$RELEASE_TAG")
@@ -11,6 +12,7 @@ CHANGELOG=$(./getChanges.sh $RELEASE_TAG $PREV_RELEASE)
 DESCRIPTION="
 **Версия**: $RELEASE_TAG
 **Создан**: $RELEASE_DATE
+**Codename**: $RELEASE_CODENAME
 **Список изменений**:
 #|
 || **Изменение** | **Коммит** | **Автор** ||
