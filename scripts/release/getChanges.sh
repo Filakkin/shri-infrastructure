d5dd9c52828fbe1c372999afa08ca1eca261cf4d
filakkin <filakkin@gmail.com>
2021-11-08 01:57:04 +0700

    Добавит в список изменения ссылки на коммиты

--- scripts/release/getChanges.sh
+++ scripts/release/getChanges.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-git log --no-merges --format="|| %<(80,trunc)%s | %<(7)(%h) | %an ||" $1...$2
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+GITHIB_LINK=$(jq -r .github $parent_path/../../config/main.json)
+git log --no-merges --format="|| %<(80,trunc)%s | %<(7)(($GITHUB_LINK/commits/%H %h)) | %an ||" $1...$2
