790c60b609ab57281482f927af7dcbb3f020457c
filakkin <filakkin@gmail.com>
2021-11-08 02:05:11 +0700

    Исправлена опечатка в наименовании переменной с URL до репозитория Github

--- scripts/release/getChanges.sh
+++ scripts/release/getChanges.sh
@@ -3,5 +3,5 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-GITHIB_LINK=$(jq -r .github $parent_path/../../config/main.json)
+GITHUB_LINK=$(jq -r .github $parent_path/../../config/main.json)
 git log --no-merges --format="|| %<(80,trunc)%s | %<(7)(($GITHUB_LINK/commits/%H %h)) | %an ||" $1...$2
