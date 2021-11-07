7548b07927bdf9130e4f97bc29e3a090cf5c294d
filakkin <filakkin@gmail.com>
2021-11-08 02:08:24 +0700

    Исправит ссылку на коммиты в github репозитории

--- scripts/release/getChanges.sh
+++ scripts/release/getChanges.sh
@@ -4,4 +4,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 GITHUB_LINK=$(jq -r .github $parent_path/../../config/main.json)
-git log --no-merges --format="|| %<(80,trunc)%s | %<(7)(($GITHUB_LINK/commits/%H %h)) | %an ||" $1...$2
+git log --no-merges --format="|| %<(80,trunc)%s | %<(7)(($GITHUB_LINK/commit/%H %h)) | %an ||" $1...$2
